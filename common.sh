ce573c2ff4ecd94881dff2d442c74f869d0fceec
Srikanth <srikanth.k1508@gmail.com>
2022-12-28 20:28:15 +0530

    cart script

--- common.sh
+++ common.sh
@@ -15,3 +15,60 @@ PRINT() {
 
 LOG=/tmp/$COMPONENT.log
 rm -f $LOG
+
+NODEJS() {
+  PRINT "Install NodeJs Repos"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG
+  STAT $?
+
+  PRINT "Install NodeJs"
+  yum install nodejs -y &>>$LOG
+  STAT $?
+
+  PRINT "Adding Application User"
+  id roboshop &>>$LOG
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>$LOG
+  fi
+  STAT $?
+
+  PRINT "Download App Content"
+  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG
+  STAT $?
+
+  PRINT "Remove Previous Version of App"
+  cd /home/roboshop &>>$LOG
+  rm -rf ${COMPONENT} &>>$LOG
+  STAT $?
+
+  PRINT "Extracting App Content"
+  unzip -o /tmp/${COMPONENT}.zip &>>$LOG
+  STAT $?
+
+  mv cart-main cart
+  cd ${COMPONENT}
+
+  PRINT "Install NODEJS Dependencies for App"
+  npm install &>>$LOG
+  STAT $?
+
+  PRINT "Configure Endpoints for SystemD configuration"
+  sed -i -e 's/REDIS_ENDPOINT/redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devopsb69.online/' /home/roboshop/${COMPONENT}/systemd.service
+  STAT $?
+
+  PRINT "Setup SystemD Properties"
+  mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+  STAT $?
+
+  PRINT "Reload SustemD"
+  systemctl daemon-reload &>>$LOG
+  STAT $?
+
+  PRINT "Restart ${COMPONENT}"
+  systemctl restart ${COMPONENT} &>>$LOG
+  STAT $?
+
+  PRINT "Enable ${COMPONENT} Service"
+  systemctl enable ${COMPONENT} &>>$LOG
+  STAT $?
+}
