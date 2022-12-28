6b7c04464a9dc66965cbe8c2f9e8b8462f79c2c1
Srikanth <srikanth.k1508@gmail.com>
2022-12-28 13:15:55 +0530

    mysql script

--- mysql.sh
+++ mysql.sh
@@ -1,12 +1,48 @@
+echo Downloading MYSQL Repo File
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
+echo Disable MySQL 8 Version repo
 dnf module disable mysql -y
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
+echo Install MYSQL
 yum install mysql-community-server -y
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
+echo Enable MySQL Service
 systemctl enable mysqld
-systemctl restart mysqld
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
+echo Start MySQL Service
+systemctl restart mysqld
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
+echo show databases | mysql -uroot -pRoboShop@1
+if [ $? -ne 0 ]
+then
 echo "ALTER USER 'root'@'locaihost' IDENTIFIED BY 'RoboShop@1';" > /tmp/root-pass-sql
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
-
 cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
+
+fi
