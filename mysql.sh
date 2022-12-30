001016fef08b97cfa5af3ad6c2c502d5cc5e658a
Srikanth <srikanth.k1508@gmail.com>
2022-12-30 16:34:35 +0530

    frontend script

--- mysql.sh
+++ mysql.sh
@@ -1,28 +1,36 @@
+if [ -z "$1" ]; then
+  echo Input argument Password is needed
+  exit
+fi
+
+COMPONENT=cart
+source common.sh
+ROBOSHOP_MYSQL_PASSWORD=$1
+
 PRINT "Downloading MYSQL Repo File"
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>$LOG
 STAT $?
 
 PRINT "Disable MySQL 8 Version repo"
-dnf module disable mysql -y
+dnf module disable mysql -y &>>$LOG
 STAT $?
 
 PRINT "Install MYSQL"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>>$LOG
 STAT $?
 
 PRINT "Enable MySQL Service"
-systemctl enable mysqld
+systemctl enable mysqld &>>$LOG
 STAT $?
 
 PRINT "Start MySQL Service"
-systemctl restart mysqld
+systemctl restart mysqld &>>$LOG
 STAT $?
 
-echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}
+echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>$LOG
 if [ $? -ne 0 ]
 then
-echo "ALTER USER 'root'@'locaihost' IDENTIFIED BY '${ROBOSHOP_MYSQL_PASSWORD}';" > /tmp/root-pass-sql
-DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
-cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
-
+  echo "ALTER USER 'root'@'locaihost' IDENTIFIED BY '${ROBOSHOP_MYSQL_PASSWORD}';" > /tmp/root-pass-sql
+  DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+  cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" &>>$LOG
 fi
